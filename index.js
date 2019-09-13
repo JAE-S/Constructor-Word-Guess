91140bf2d0dec6ffc502e71150f7daf7d73452a7
Jessie S <jessie.sholler@gmail.com>
2019-09-13 17:44:13 -0400

    Start game function added - propmt user guess function added

--- index.js
+++ index.js
@@ -1,10 +1,107 @@
-// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+// ─────── Node Packages ─────── // 
+var chalk = require('chalk');
+var inquirer = require("inquirer");
+
+// ─────── Require word.js ─────── // 
+// var word = require('./word.js'); 
+
+// ─────── Global Variables ─────── // 
+var guess; 
+var correctLetters = []; 
+var remaingGuesses = 5; 
+var losses = 0; 
+var wins = 0; 
+var correctWord; 
+var gameOver = true; 
+
+var wordList = ["PLUM", 
+                "MAGENTA",
+                "RUBY", 
+                "CLEMENTINE", 
+                "DAISY"]
+           
+    console.log(chalk.cyan(`
+    ───────────────────────────────────────────────────────────────────────────────────
+                                        WELCOME TO
+    ───────────────────────────────────────────────────────────────────────────────────
+
+       ●           ●   ● ● ●   ● ● ●   ● ● ●     ● ●    ●     ●  ● ● ●   ● ●    ● ●
+        ●         ●   ●     ●  ●    ●  ●    ●   ●       ●     ●  ●      ●      ●
+         ●   ●   ●    ●     ●  ● ● ●   ●    ●   ●  ● ●  ●     ●  ● ●     ● ●    ● ●
+          ● ● ● ●     ●     ●  ●   ●   ●    ●   ●    ●  ●     ●  ●          ●      ●
+           ●   ●       ● ● ●   ●    ●  ● ● ●      ● ●    ● ● ●   ● ● ●   ● ●    ● ●
+
+    ───────────────────────────────────────────────────────────────────────────────────
+                                    COLOR EDITION 
+    ───────────────────────────────────────────────────────────────────────────────────
+    \n`));
+    
+start()
+// Start a new game promt 
+function start(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to start a new game?",
+            choices: ["Yes", "No"],
+            name: "start"
+        }
+    ]).then(function(response) {
+        
+        if (response.start === "Yes"){ 
+            console.log("\n            Ready to play             \n")
+        } else {
+            console.log("\n                  ㋡                 \n")
+        }
+    
+    })
+}
 
 
 // Randomly selects a word and uses the Word constructor to store it
+function start(){
+    if (gameOver === true){
+        correctLetters = []; 
+        wrongLetters = [];
+        remaingGuesses = 5;      
+    }
+    gameOver = false; 
+
+    currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+    correctLetters = new Word(currentWord); 
+    userGuess();
+}
+
+
 // Prompts the user for each guess and keeps track of the user's remaining guesses
+function userGuess() {
 
+  
 
+        // Asks user to guess a letter 
+        if(remaingGuesses >= 5) {
+            inquirer.prompt([
+                {
+                type: "input",
+                message: "Guess a letter", 
+                name: "guess"
+                }
+            ]).then(function(response) {
+                var regex = /^[a-z]+$/i;
+                
+                if (response.guess.length === 1 && regex.test(response.guess)) {
+                    guess = response.guess.toUpperCase();
+                console.log(guess); 
+                } else {
+                    console.log("\n Invalid. Please enter a letter A - Z.\n")
+                    userGuess(); 
+                // } else {
+                // console.log("GAME OVER")
+                }   
+            });
+
+        }
+} 
 
 
 
