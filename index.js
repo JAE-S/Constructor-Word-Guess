565424aa06f5d857d23bd72c968ea515c2c29afd
Jessie S <jessie.sholler@gmail.com>
2019-09-14 23:56:26 -0400

    progress

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var chalk = require('chalk');
 var inquirer = require("inquirer");
 
 // ─────── Require word.js ─────── // 
-// var word = require('./word.js'); 
+var Word = require('./word.js'); 
+
 
 // ─────── Global Variables ─────── // 
 var guess; 
@@ -14,95 +15,109 @@ var wins = 0;
 var correctWord; 
 var gameOver = true; 
 
-var wordList = ["PLUM", 
-                "MAGENTA",
-                "RUBY", 
-                "CLEMENTINE", 
-                "DAISY"]
+
            
     console.log(chalk.cyan(`
-    ───────────────────────────────────────────────────────────────────────────────────
-                                        WELCOME TO
-    ───────────────────────────────────────────────────────────────────────────────────
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                     WELCOME TO
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
-       ●           ●   ● ● ●   ● ● ●   ● ● ●     ● ●    ●     ●  ● ● ●   ● ●    ● ●
-        ●         ●   ●     ●  ●    ●  ●    ●   ●       ●     ●  ●      ●      ●
-         ●   ●   ●    ●     ●  ● ● ●   ●    ●   ●  ● ●  ●     ●  ● ●     ● ●    ● ●
-          ● ● ● ●     ●     ●  ●   ●   ●    ●   ●    ●  ●     ●  ●          ●      ●
-           ●   ●       ● ● ●   ●    ●  ● ● ●      ● ●    ● ● ●   ● ● ●   ● ●    ● ●
-
-    ───────────────────────────────────────────────────────────────────────────────────
-                                    COLOR EDITION 
-    ───────────────────────────────────────────────────────────────────────────────────
-    \n`));
+                        ❍   ___   ❍     ▁▁▁▁        ▁▁▁▁         ▁▁▁▁        ▁    ▁  ❍   ▁▁▁▁▁  ❍   ▁▁▁▁       ▁▁▁     
+     __         ___      〵ᐟ _ 〵╱   ❍ ┃  _ 〵 ❍   ┃  _ 〵   ❍  ᐟ ___| ❍  ❍ ┃ ┃❍ | |  〵┃ ____|/  /   __| ❍  /  __| ❍  
+     ╲  ╲      ╱  ╱       ┃ ┃ | │     ╲┃ ┃▁) |/   /┃ ┃ | |   〵┃ ┃  _ /   〵┃ ┃〵| |    ┃  _|    <╲▁▁  ╲/   <╲▁▁  ╲/   
+    ╱ ╲  ╲ ╱╲ ╱  ╱╲  .-. _┃ ┃▁| │      ┃  ▁ <    ❍ ┃ ┃▁| |╲    ┃ ┃▁| |      ┃ ┃▁ | |    ┃ ┃▁▁▁    ❍▁▁▁) |    ❍ ▁▁) |   
+   ❍   ╲        ╱  ❍ 〵_)-〵▁▁  ᐟ      ┃▁┃ ╲▁╲     ┃▁▁▁▁ ᐟ ❍   〵▁▁▁▁|    <<〵▁▁▁▁ ᐟ    ┃▁▁▁▁▁|   |▁▁▁▁ />>   |▁▁▁▁/>>  
+    .-._╲  ╱╲  ╱_.-.         ヽヽ      //   ╲╲▁     |||▁        ▁)(|▁    (__)  )(       <<   >>    )(   (__)   )(   (__) 
+    〵_)─       ─(_/          (__)    (__)  (__)   (__)_)      (__)__)        (__)     (__) (__)  (__)        (__) 
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                    COLOR EDITION 
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+    \n`)); 
     
-start()
+newGame()
 // Start a new game promt 
-function start(){
+function newGame(){
     inquirer.prompt([
         {
             type: "list",
-            message: "Would you like to start a new game?",
+            message: chalk.cyan.dim("Would you like to start a new game? \n ──────────────────────────────────⫸ "),
             choices: ["Yes", "No"],
             name: "start"
         }
     ]).then(function(response) {
         
         if (response.start === "Yes"){ 
-            console.log("\n            Ready to play             \n")
+            console.log(chalk.greenBright("\n          ٩(˘◡˘)۶ Ready to play             \n"))
+            start();
         } else {
-            console.log("\n                  ㋡                 \n")
+            console.log(chalk.redBright("\n                ¯〵_(ツ)_〳¯                 \n"))
         }
     
     })
 }
 
+  //───────────────────────────────────────────────────────────────────────────// 
+ //                          RANDOMLY SELECTS A WORD                          //
+//───────────────────────────────────────────────────────────────────────────// 
 
-// Randomly selects a word and uses the Word constructor to store it
-function start(){
-    if (gameOver === true){
-        correctLetters = []; 
-        wrongLetters = [];
-        remaingGuesses = 5;      
-    }
-    gameOver = false; 
+// Stores word list 
+var wordList = ["PLUM", 
+                "MAGENTA",
+                "RUBY", 
+                "CLEMENTINE", 
+                "DAISY"];
 
-    currentWord = wordList[Math.floor(Math.random() * wordList.length)];
-    correctLetters = new Word(currentWord); 
-    userGuess();
-}
-
-
-// Prompts the user for each guess and keeps track of the user's remaining guesses
-function userGuess() {
-
-  
-
-        // Asks user to guess a letter 
-        if(remaingGuesses >= 5) {
-            inquirer.prompt([
-                {
-                type: "input",
-                message: "Guess a letter", 
-                name: "guess"
-                }
-            ]).then(function(response) {
-                var regex = /^[a-z]+$/i;
-                
-                if (response.guess.length === 1 && regex.test(response.guess)) {
-                    guess = response.guess.toUpperCase();
-                console.log(guess); 
-                } else {
-                    console.log("\n Invalid. Please enter a letter A - Z.\n")
-                    userGuess(); 
-                // } else {
-                // console.log("GAME OVER")
-                }   
-            });
-
+// // Randomly selects a word and uses the Word constructor to store it
+    function start(){
+        if (gameOver === true){
+            correctLetters = []; 
+            wrongLetters = [];
+            remaingGuesses = 5;      
         }
-} 
+        gameOver = false; 
 
+        currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+        console.log(currentWord)
+        correctLetters = new Word(currentWord); 
+        console.log(correctLetters); 
+        userGuess();
+    }
+
+  //───────────────────────────────────────────────────────────────────────────// 
+ //                      PROMPTS THE USER FOR EACH GUESS                      //
+//───────────────────────────────────────────────────────────────────────────// 
+  
+    function userGuess() {
+    // Prompts the user for each guess and keeps track of the user's remaining guesses
+            // Asks user to guess a letter 
+            if(remaingGuesses >= 5) {
+                inquirer.prompt([
+                    {
+                    type: "input",
+                    message: "Guess a letter!", 
+                    name: "guess"
+                    }
+                ]).then(function(response) {
+                    var regex = /^[a-z]+$/i;
+                    
+                    if (response.guess.length === 1 && regex.test(response.guess)) {
+                        guess = response.guess.toUpperCase();
+                            // Word.checkGuess(answer.guess);
+                    // console.log(guess); 
+                    } else {
+                        console.log("\n Invalid. Please enter a letter A - Z.\n")
+                        userGuess(); 
+                    // } else {
+                    // console.log("GAME OVER")
+                    }   
+                });
+
+            }
+    } 
+
+  //───────────────────────────────────────────────────────────────────────────// 
+ //                   CHECKS IF USERGUESS MATCHES A LETTER                    //
+//───────────────────────────────────────────────────────────────────────────// 
 
 
 // Letter.js should not require any other files.
--- letter.js
+++ letter.js
@@ -1,9 +1,37 @@
-// Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+// // Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+var chalk = require('chalk');
 
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+// // Letter constructor 
+var Letter = function(LETTER){
+    
+     // A string value to store the underlying character for the letter
+    this.letter = LETTER; 
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false; 
+
+    this.display = function() {
+        if(this.guessed === true){
+            return LETTER; 
+        } else {
+            return "_"; 
+        }
+    }
+
+    this.checkAnswer = function(userGuess) {
+        if (userGuess === LETTER){
+            this.guessed = true; 
+        } else {
+            this.guessed = false; 
+        }
+        return this.guessed;
+    };
+};
+
+module.exports = Letter;
+
+
+
+
 
                               
 
